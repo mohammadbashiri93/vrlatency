82fcf59ec380b2a6349c8d8572870fdf4f1513c4
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-09-17 18:12:37 +0200

    changes to notebook, as well as changing samplesize for arduino mega

--- run_display_experiment.bat
+++ run_display_experiment.bat
@@ -1,2 +1,2 @@
 call activate vrlatency3
-call measure_latency display --port COM9 --trials 100 --jitter --stimsize 200 --screen 1 --singlemode --output Z:\\nickdg\vrlatency
+call measure_latency display --port COM11 --trials 20 --jitter --nsamples 400 --stimsize 1000 --screen 1 --singlemode --output C:\Users\sirotalab\Desktop\Measurement
